330170fabd4d0c8bd9644c28c0316daff05d0ee7
Tian <dingtian2019@163.com>
2020-01-29 12:08:29 -0500

    update new.py

--- new.py
+++ new.py
@@ -1,6 +1,5 @@
 def function():
 	print('hello the wolrd')
-<<<<<<< HEAD
 	pass
 
 def plus(x, y):
@@ -8,6 +7,5 @@ def plus(x, y):
 
 def minus(x,y):
 	return x-y
-=======
 	pass
->>>>>>> 72d90b494f7da3bdc83fcfac70732eb924706ee7
+
